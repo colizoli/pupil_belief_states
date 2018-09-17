f1f81f8c5848e3cef6aa35df0a06f941f2086b67
Olympia Colizoli <olympia.colizoli@gmail.com>
2018-09-17 19:29:19 +0200

    Update pupil_preprocessing_control.py

--- analysis/control_task/pupil_preprocessing_control.py
+++ analysis/control_task/pupil_preprocessing_control.py
@@ -33,9 +33,7 @@ from itertools import chain
 import logging
 import logging.handlers
 import logging.config
-
 import copy
-
 from IPython import embed as shell
 
 sys.path.append(os.environ['ANALYSIS_HOME'])
@@ -43,8 +41,8 @@ from Tools.log import *
 from Tools.Operators import ArrayOperator, EDFOperator, HDFEyeOperator, EyeSignalOperator
 from Tools.Operators.EyeSignalOperator import detect_saccade_from_data
 from Tools.Operators.CommandLineOperator import ExecCommandLine
-from Tools.other_scripts import functions_jw as myfuncs
-from Tools.other_scripts import functions_jw_GLM as GLM
+from Tools.other_scripts import myfuncs as myfuncs
+# from Tools.other_scripts import functions_jw_GLM as GLM
 
 matplotlib.rcParams['pdf.fonttype'] = 42
 matplotlib.rcParams['ps.fonttype'] = 42
